0dc3b8b327fae294fd6a52d01215b44218563d7e
Ally Summers <ally@summers.pw>
2025-11-01 18:25:45 +1100

    fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,25 @@
 // @ts-check
-import  eslint from "@eslint/js";
-import { defineConfig } from "eslint/config";
-import tseslint from "typescript-eslint";
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.strictTypeChecked,
-  tseslint.configs.stylisticTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
+	eslint.configs.recommended,
+	tseslint.configs.strictTypeChecked,
+	tseslint.configs.stylisticTypeChecked,
+	{
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
+	{
+		rules: {
+			// https://github.com/typescript-eslint/typescript-eslint/issues/11732
+			'@typescript-eslint/unified-signatures': 'off',
+			'@typescript-eslint/no-misused-promises': 'off',
+		},
+	},
 );
